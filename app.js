bd70d02d90d78c13487e4e5dac710339723b9569
Sri Vyshnavi Kshatriya <vyshnavisri1908@gmail.com>
2022-10-26 18:05:48 -0500

    new enpoint

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/mydata', indexRouter);
-app.use('/users', usersRouter);
+app.use('/mydata', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
