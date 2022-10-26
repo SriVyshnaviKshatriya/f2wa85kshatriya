acd05f56c201daa7423d2aa04951c6401bf90095
Sri Vyshnavi Kshatriya <vyshnavisri1908@gmail.com>
2022-10-26 18:18:22 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('my data', { title: 'Sri Vyshnavi Kshatriya' });
+  res.render('mydata', { title: 'Sri Vyshnavi Kshatriya' });
 });
 
 module.exports = router;
